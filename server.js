ccf780dbe312be0c40c59ff4c4330804fafcde35
KilianHemery <kilian85130@hotmail.fr>
2017-09-19 12:00:36 +0200

    Affichage de la liste de jouets

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
-//const Jouet = require('./jouet.js');
-//const TrancheAge = require('./trancheAge.js');
-//const Categorie = require('./categorie');
 
 const express = require('express');
 const app = express();
 
-//console.log("Bienvenue dans le catalogue de jouet");
+const Jouet = require('./jouet.js');
+const TrancheAge = require('./trancheAge.js');
+const Categorie = require('./categorie.js');
 
-app.get('/', function (req, res) {
-    res.send('Bienvenue dans le catalogue de jouet');
-});
-
-/*let tranche25 =  new TrancheAge("tranche25",2,5);
-console.log(tranche25.toString());
+// DONNEES
+let tranche02 =  new TrancheAge("tranche02",0,2);
+let tranche25 =  new TrancheAge("tranche25",2,5);
+let tranche48 =  new TrancheAge("tranche48",4,8);
 
 let categorie1 = new Categorie("Plein Air");
-console.log(categorie1.libelle);
+let categorie2 = new Categorie("Jeux d'imagination");
+let categorie3 = new Categorie("Jeux d'eveil");
 
-let unJouet = new Jouet("Draisienne junior bleue",tranche25,categorie1);
-console.log(unJouet.trancheAge);
+let lesJouets = [];
+let unJouet1 = new Jouet("Draisienne junior bleue",tranche25,categorie1);
+lesJouets.push(unJouet1);
 
-categorie1.afficherJouets(console.log);*/
+let unJouet2 = new Jouet("Trottinette 2 en 1 space Blanche",tranche48, categorie1);
+lesJouets.push(unJouet2);
 
-app.listen(8081);
+let unJouet3 = new Jouet("Atelier de bricolage Tap tap Véhicules",tranche48,categorie2);
+lesJouets.push(unJouet3);
+
+//ROUTES
+
+app.get('/', function (req, res) {
+    let responseText = 'Bienvenue dans le catalogue de jouet';
+    res.send(responseText);
+});
+
+//affiche tous les élèves
+app.get('/jouets',
+    (req,res) =>  {
+           let responseText = 'Voici la liste des jouets </br>';
+           lesJouets.forEach(
+               (unJouet) => {responseText += `Jouet : ${unJouet.libelle}  </br>`;}
+           );
+           res.send(responseText);
+       }
+);
+
+// DEMARRAGE DU SERVEUR
+try {
+    app.listen(8081,'127.0.0.1',() => {
+        console.log("Serveur demarré");
+    });
+} catch (err) {
+    console.error(' Erreur de démarrage du serveur!', err);
+}
