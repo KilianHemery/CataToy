4a2357bc2c203c931b34da860ea7bccbdacb6883
KilianHemery <kilian85130@hotmail.fr>
2017-09-19 11:17:53 +0200

    Création de la classe categorie

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Jouet = require('./jouet.js');
 const TrancheAge = require('./trancheAge.js');
+const Categorie = require('./categorie');
 
 console.log("Bienvenue dans le catalogue de jouet");
 
@@ -8,6 +9,9 @@ console.log(unJouet.libelle);
 
     let tranche25 =  new TrancheAge("tranche25",2,5);
 console.log(tranche25.toString());
+
+let categorie1 = new Categorie("Plein Air");
+console.log(categorie1.libelle);
 
 let unJouet = new Jouet("Draisienne junior bleue",tranche25);
 console.log(unJouet.trancheAge);
